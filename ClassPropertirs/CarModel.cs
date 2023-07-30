a90acc2976c12759c0ef189eb1029f29882e8358
Mrmeysam2 <mr.meysam2@gmail.com>
2023-07-30 16:32:11 +0330

    Add project files.

---
+++ ClassPropertirs/CarModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPropertirs
+{
+    class CarModel
+    {
+        List<string> lsColor = new List<string>() { "blue", "red", "yellow" };
+
+        public string Name;
+        public string ManuFacturer;
+        public string color;
+         
+        public CarModel(string name, string manufacturer, string color)
+        {
+            Name = name;
+            ManuFacturer = manufacturer;
+            Color = color;
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set
+            {
+                if (lsColor.Contains(value))
+                {
+                    color = value;
+                }
+                else
+                    color = "ND";
+            }
+        }
+    }
+}
---
+++ ClassPropertirs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassPropertirs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarModel carModel1 = new CarModel("Perayd", "Saipa", "blue");
+            CarModel carModel2 = new CarModel("G650", "Benz", "red");
+
+            Console.WriteLine(carModel1.Color);
+            Console.WriteLine(carModel2.Color);
+
+            Console.ReadLine();
+        }
+    }
+}
