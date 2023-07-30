01ca1379cee3d5bfa0837eb23233e078599a6b58
Mrmeysam2 <mr.meysam2@gmail.com>
2023-07-30 16:32:41 +0330

    One Change

--- ClassPropertirs/Program.cs
+++ ClassPropertirs/Program.cs
@@ -8,7 +8,7 @@ namespace ClassPropertirs
         {
             CarModel carModel1 = new CarModel("Perayd", "Saipa", "blue");
             CarModel carModel2 = new CarModel("G650", "Benz", "red");
-
+             
             Console.WriteLine(carModel1.Color);
             Console.WriteLine(carModel2.Color);
 
